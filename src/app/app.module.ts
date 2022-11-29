596f62d866846fd4404ea292790acbac7dd99dde
itsmeabhishekn <abhishekn4454@gmail.com>
2022-11-29 15:33:29 +0530

    Added apis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewComponent } from './view/view.component';
+import { RouterModule,Routes } from '@angular/router';
+
+const myRoutes:Routes = [
+  {
+    path:"",
+    component:ViewComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(myRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
